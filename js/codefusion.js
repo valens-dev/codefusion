663ee4482e5b114901edfb21d166674bd3668c7a
Karlo Andric <karloandric81@gmail.com>
2024-03-28 12:17:15 +0100

    IP address change test

---
+++ js/codefusion.js
@@ -0,0 +1,13 @@
+
+const toggleLanguageButton = $(".language-switcher");
+const rtlStylesheet = 'css/main-RTL.css';
+const ltrStylesheet = 'css/main-LTR.css';
+
+toggleLanguageButton.on("click", function () {
+  const stylesheetLink = $('link.language-form');
+
+  const currentStylesheet = stylesheetLink.attr('href');
+  const newStylesheet = currentStylesheet === rtlStylesheet ? ltrStylesheet : rtlStylesheet;
+
+  stylesheetLink.attr('href', newStylesheet);
+});
